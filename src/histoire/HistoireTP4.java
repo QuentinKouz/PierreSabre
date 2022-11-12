a249bf2c98b4a99d4c44a41f7c2adcf97f2b9562
Quentin Kouzmitch <quentinvegeto@gmail.com>
2022-11-12 14:53:39 +0100

    CommercantFinit

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,9 +1,14 @@
 package histoire;
 
 import personnages.Commercant;
+import personnages.Humain;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
 		Commercant marco = new Commercant("Marco", "thé", 20);
+		marco.direBonjour();
+		marco.seFaireExtorquer(20);
+		marco.recevoir(15);
+		marco.boire();
 	}
 }
