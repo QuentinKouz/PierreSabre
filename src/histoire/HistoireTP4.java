0d574143886ff6c791fe47b412e89fc444d5ea1a
Quentin Kouzmitch <quentinvegeto@gmail.com>
2022-11-12 15:28:28 +0100

    RoninFinit

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,14 +2,16 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Humain;
+import personnages.Ronin;
 import personnages.Yakuza;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
 		Commercant marco = new Commercant("Marco", "thé", 20);
 		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
-		yaku.direBonjour();
-		yaku.extorquer(marco);
+		Ronin roro = new Ronin("Roro", "shochu" ,60);
+		roro.direBonjour();
+		roro.donner(marco);
 		
 	}
 }
